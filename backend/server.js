474e6caeb71acd40b029f86adc4f284d0585eb71
amanjagdev <amankumarjagdev@gmail.com>
2020-03-25 13:22:38 +0530

    made routing and added add product api

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const keys = require('./keys/keys')
-const bodyParser = require('body-parser');
-const passport = require('passport')
-const cookieSession = require('cookie-session')
 const cors = require('cors')
 
 
@@ -20,16 +17,8 @@ mongoose.set('useCreateIndex', true);
 
 
 
-// passport
-require('./auth/passport')(passport)
-
 // Loading all the api
-const userApi = require('./api/user/user')
-const authApi = require('./api/auth/auth')
-const stripeApi = require('./api/payment/stripe')
-const productApi = require('./api/product/product')
-const bidApi = require('./api/bidding/bidding')
-const searchApi = require('./api/search/search')
+const productApi = require('./api/addproduct')
 
 // variables initialization
 const app = express()
@@ -41,38 +30,13 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// PASSPORT 
-app.use(
-    cookieSession({
-        maxAge: 14*24*60*1000,
-        keys: [keys.CookieSecret]
-    })
-)
-app.use(passport.initialize())
-app.use(passport.session())
-
 // API IN APP
-app.use('/auth', authApi)
-app.use('/api/user', userApi)
-app.use('/api/payment', stripeApi)
-app.use('/api/product', productApi)
-app.use('/api/bid', bidApi)
-app.use('/api/search', searchApi)
+app.use('/api/addproduct', productApi)
 
 
 app.get('/', (req, res) => {
     res.json({
-        msg: 'user logged in'
-    })
-})
-
-
-app.get('/fail', (req, res)=>{
-    console.log("FAILURE REDIRECT >>>>")
-    res
-    .status(400)
-    .json({
-        msg: "Wrong Credentials"
+        msg: 'Api is working'
     })
 })
 
